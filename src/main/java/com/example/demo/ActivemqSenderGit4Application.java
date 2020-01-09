d0fc8e7924182f6ed5b48f61f0a29d48516a6f4a
bbicer <bbicer@heas0973.sgairport.local>
2020-01-10 00:03:14 +0300

    first commit

---
+++ src/main/java/com/example/demo/ActivemqSenderGit4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqSenderGit4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqSenderGit4Application.class, args);
+	}
+
+}
